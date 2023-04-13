147d763bf670e1f394d6512b00a36a27cbaf8967
NealAites <aitesneal@gmail.com>
2023-04-13 15:42:35 -0500

    added CreateActivityActivityTest and ActivityDaoTest

--- MusicPlaylistServiceLambda/src/main/java/VacanzaLambda/src/main/java/musicplaylistservice/dependency/ServiceComponent.java
+++ MusicPlaylistServiceLambda/src/main/java/VacanzaLambda/src/main/java/musicplaylistservice/dependency/ServiceComponent.java
@@ -26,4 +26,6 @@ public interface ServiceComponent {
      * @return SearchItinerariesActivity
      */
     SearchItinerariesActivity provideSearchItinerariesActivity();
+
+    CreateActivityActivity provideCreateActivityActivity();
 }
