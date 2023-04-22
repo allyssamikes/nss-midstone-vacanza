0da84acaede626a7e7edbfbc42cc5982bdeb078b
Erin Geier <erin.geier@gmail.com>
2023-04-22 08:26:57 -0500

    added removeActivity button/function to addActivityToItinerary page. will rename.

--- web/src/pages/addActivityToItinerary.js
+++ web/src/pages/addActivityToItinerary.js
@@ -9,7 +9,7 @@ import DataStore from '../util/DataStore';
 class AddActivityToItinerary extends BindingClass {
     constructor() {
         super();
-        this.bindClassMethods(['mount', 'submit'], this);
+        this.bindClassMethods(['mount', 'submit','submitRemove'], this);
         this.dataStore = new DataStore();
         this.header = new Header(this.dataStore);
     }
@@ -18,7 +18,7 @@ class AddActivityToItinerary extends BindingClass {
      */
     mount() {
         document.getElementById('add-activity').addEventListener('click', this.submit);
-
+        document.getElementById('remove-activity').addEventListener('click', this.submitRemove);
         this.header.addHeaderToPage();
 
         this.client = new MusicPlaylistClient();
@@ -60,6 +60,44 @@ class AddActivityToItinerary extends BindingClass {
             activityInput2.value = "";
             createButton.innerText = 'Complete';
             createButton.innerText = 'Add Another Activity';
+        }
+        /**
+         * Method to run when the remove activity submit button is pressed. Calls the VacanzaService to remove the
+         * activity.
+         */
+        async submitRemove(evt) {
+            evt.preventDefault();
+
+            const errorMessageDisplay = document.getElementById('error-message');
+            errorMessageDisplay.innerText = ``;
+            errorMessageDisplay.classList.add('hidden');
+
+            const createButton = document.getElementById('remove-activity');
+            const origButtonText = createButton.innerText;
+            createButton.innerText = 'Loading...';
+
+            const cityCountry = document.getElementById('activity-cityCountry').value;
+            const name = document.getElementById('activity-name').value;
+
+            const email = document.getElementById("email").value;
+            const tripName = document.getElementById("itinerary-name").value;
+
+            const activities = await this.client.removeActivityFromItinerary(email, tripName, cityCountry, name, (error) => {
+                createButton.innerText = origButtonText;
+                const errorMessageDisplay = document.getElementById('error-message');
+                errorMessageDisplay.innerText = `Error: ${error.message}`;
+                errorMessageDisplay.classList.remove('hidden');
+            });
+
+            this.dataStore.set('activityList', activities);
+            const itinerary = await this.client.getItinerary(email, tripName);
+            this.dataStore.set('itinerary', itinerary);
+            let activityInput1 = document.getElementById('activity-cityCountry');
+            activityInput1.value = "";
+            let activityInput2 = document.getElementById('activity-name');
+            activityInput2.value = "";
+            createButton.innerText = 'Complete';
+            createButton.innerText = 'Remove Another Activity';
         }
 }
 
