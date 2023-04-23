a3f6099bcd640d618bb6ed4cadabf823796a4006
Erin Geier <erin.geier@gmail.com>
2023-04-23 09:50:25 -0500

    log statement add to Lambda. Path trial was abandoned

--- MusicPlaylistServiceLambda/src/main/java/VacanzaLambda/src/main/java/musicplaylistservice/lambda/SearchActivitiesLambda.java
+++ MusicPlaylistServiceLambda/src/main/java/VacanzaLambda/src/main/java/musicplaylistservice/lambda/SearchActivitiesLambda.java
@@ -5,13 +5,16 @@ import VacanzaLambda.src.main.java.musicplaylistservice.activity.results.SearchA
 import VacanzaLambda.src.main.java.musicplaylistservice.dependency.ServiceComponent;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class SearchActivitiesLambda extends LambdaActivityRunner<SearchActivitiesRequest, SearchActivitiesResult>
             implements RequestHandler<LambdaRequest<SearchActivitiesRequest>, LambdaResponse> {
-
+private final Logger log = LogManager.getLogger();
     @Override
     public LambdaResponse handleRequest(LambdaRequest<SearchActivitiesRequest> input, Context context) {
- //       SearchActivitiesRequest unauthenticatedRequest = input.fromBody(SearchActivitiesRequest.class);
+        log.info("Lambda handleRequest");
+        //       SearchActivitiesRequest unauthenticatedRequest = input.fromBody(SearchActivitiesRequest.class);
         return super.runActivity(
                 () -> input.fromQuery(query ->
                         SearchActivitiesRequest.builder()
